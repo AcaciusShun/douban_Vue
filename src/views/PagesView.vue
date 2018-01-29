87771b05b252d37fe5a9a050b1d14f1dcb35d3f2
bailonghsu <35856964+bailonghsu@users.noreply.github.com>
2018-01-29 21:31:15 +0800

    add PagesViews`
    添加综合视图

---
+++ src/views/PagesView.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="pages-view"></div>	
+	
+</template>
+
+<script>
+	import HeaderBar from '../components/HeaderBar.vue'
+	// import TalionVi
+	
+
+</script>
+
+<style scoped>
+	
+
+
+</style>
