3923b13cb8bc2f93da5c130c7f28657ec8a3ef40
bailonghsu <35856964+bailonghsu@users.noreply.github.com>
2018-01-30 20:44:59 +0800

    update Loading
    更新 加载组件

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,28 +1,25 @@
 <template>
-	<div class="loading">
-		<img src="../assets/loading_green.gif" height="80" width="80" alt="loading">
-	</div>
+  <div class="loading">
+    <img src="../assets/loading_green.gif" alt="loading">
+  </div>
 </template>
-
 
 <script>
 export default {
-	name: 'loading',
-	data(){
-		return{}
-	}
-}	
+  name: 'loading',
+  data () {
+    return {}
+  }
+}
 </script>
 
-
 <style lang="scss" scoped>
-	.loading{
-		margin: 2rem;
-		text-align: center;
-		
-		img{
-			width: 4.8rem;
-		}
-	}
+.loading {
+  margin: 2rem;
+  text-align: center;
 
+  img {
+    width: 4.8rem;
+  }
+}
 </style>
