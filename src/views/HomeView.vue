2ffb4890c58f9b3894d514a3ab1709df4bc05164
bailongctui <bailongctui@gmail.com>
2018-01-30 20:04:48 +0800

    add HomeView
    创建主页视图

---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+	
+
+
+
+
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+
+import InfiniteLoading from 'vue-infinite-loading'
+import SubNav from '../components/SubNav'
+import List from '../components/List'
+import Loading from '../components/Loading'
+
+export default {
+  name: 'home-view',
+  components: { SubNav, List, InfiniteLoading, Loading },
+  data () {
+    return {}
+  },
+
+  computed: {
+
+  },
+  methods: {
+
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.sub-nav {
+  margin: 0 1.8rem;
+  padding-top: 0.2rem;
+}
+</style>
