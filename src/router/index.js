1125754a36df30f39968da55bf282997b8976913
bailongctui <bailongctui@gmail.com>
2018-01-31 16:12:09 +0800

    更新 路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,15 @@ import Router from 'vue-router'
 
 import PagesView from '../views/PagesView'
 import HomeView from '../views/HomeView'
-// import MovieView from '../views/MovieView'
-// import BookView from '../views/BookView'
+import MovieView from '../views/MovieView'
+import BookView from '../views/BookView'
 import StatusView from '../views/StatusView'
-// import GroupView from '../views/GroupView'
+import GroupView from '../views/GroupView'
 import SubjectView from '../views/SubjectView'
 import DetailView from '../views/DetailView'
-// import SearchView from '../views/SearchView'
-// import LoginView from '../views/LoginView'
-// import RegisterView from '../views/RegisterView'
+import SearchView from '../views/SearchView'
+import LoginView from '../views/LoginView'
+import RegisterView from '../views/RegisterView'
 
 Vue.use(Router)
 
@@ -34,26 +34,26 @@ export default new Router({
           name: 'HomeView',
           component: HomeView
         },
-        // {
-        //   path: 'movie',
-        //   name: 'MovieView',
-        //   component: MovieView
-        // },
-        // {
-        //   path: 'book',
-        //   name: 'BookView',
-        //   component: BookView
-        // },
+        {
+          path: 'movie',
+          name: 'MovieView',
+          component: MovieView
+        },
+        {
+          path: 'book',
+          name: 'BookView',
+          component: BookView
+        },
         {
           path: 'status',
           name: 'StatusView',
           component: StatusView
         },
-        // {
-        //   path: 'group',
-        //   name: 'GroupView',
-        //   component: GroupView
-        // },
+        {
+          path: 'group',
+          name: 'GroupView',
+          component: GroupView
+        },
         {
           path: 'detail/:id',
           name: 'DetailView',
@@ -69,23 +69,23 @@ export default new Router({
         subject: SubjectView
       }
     },
-    // {
-    //   path: '/search',
-    //   name: 'SearchView',
-    //   components: {
-    //     default: PagesView,
-    //     search: SearchView
-    //   }
-    // },
-    // {
-    //   path: '/login',
-    //   name: 'LoginView',
-    //   component: LoginView
-    // },
-    // {
-    //   path: '/register',
-    //   name: 'RegisterView',
-    //   component: RegisterView
+    {
+      path: '/search',
+      name: 'SearchView',
+      components: {
+        default: PagesView,
+        search: SearchView
+      }
+    },
+    {
+      path: '/login',
+      name: 'LoginView',
+      component: LoginView
+    },
+    {
+      path: '/register',
+      name: 'RegisterView',
+      component: RegisterView
     // },
     {
       path: '*',
