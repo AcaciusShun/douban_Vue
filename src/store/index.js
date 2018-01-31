2fe5b16deb94cab4aa2e1648e605c8c1228cc706
bailongctui <bailongctui@gmail.com>
2018-01-31 12:56:14 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import movie from './modules/movie'
 import activities from './modules/activities'
-import book from './modules/book'
+// import movie from './modules/movie'
+// import book from './modules/book'
 import subject from './modules/subject'
-import group from './modules/group'
-import search from './modules/search'
-import user from './modules/user'
+// import group from './modules/group'
+// import search from './modules/search'
+// import user from './modules/user'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    movie,
+    // movie,
     activities,
-    book,
+    // book,
     subject,
-    group,
-    search,
-    user
+    // group,
+    // search,
+    // user
   }
 })
