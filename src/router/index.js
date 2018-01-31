18f629185eda63bc8caac8b6aadf32cba033568a
bailongctui <bailongctui@gmail.com>
2018-01-31 16:12:44 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -86,7 +86,7 @@ export default new Router({
       path: '/register',
       name: 'RegisterView',
       component: RegisterView
-    // },
+    },
     {
       path: '*',
       redirect: '/pages/'
